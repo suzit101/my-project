7feed17526ad9b4c356d48a03d3c21052210b756
sujit101 <sujittamang5@gmail.com>
2024-10-05 04:35:53 -0600

    about-us and contact-us page content changed

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -4,6 +4,12 @@ const AboutUs = () => {
   return (
     <div>
       <div>This is my about page.</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+        reiciendis delectus hic deleniti numquam earum. Maxime voluptatibus,
+        itaque harum quo quisquam perferendis, impedit esse voluptatem sint,
+        repudiandae praesentium tenetur illo.
+      </p>
     </div>
   );
 };
--- src/pages/contact-us.jsx
+++ src/pages/contact-us.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam nam
+        eius enim vero accusamus architecto deserunt officiis, possimus, ullam
+        illum cum eos nihil aliquam. Iusto tempore quaerat iure atque autem
+        accusamus? Tenetur obcaecati odit ex facilis deserunt consequuntur
+        quidem voluptatum id, deleniti saepe nisi porro rem a exercitationem!
+        Quasi, odio.
+      </p>
+    </div>
+  );
 };
 
 export default ContactUs;
